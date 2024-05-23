87587bb992b52706577c6eaf58ef2ad8c69b206c
danyalahmad2000 <danigurrero.dg@gmail.com>
2024-05-23 23:13:49 +0500

    Contact Us Text white

--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -28,7 +28,7 @@ const ContactUs = () => {
   return (
     <section id="contactUs" ref={contactUsRef} className="bg-gray-900">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className={`mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white ${isVisible ? 'slide-from-left' : ''}`}>
+        <h2 className={`mb-4 text-4xl tracking-tight font-extrabold text-center text-white dark:text-white ${isVisible ? 'slide-from-left' : ''}`}>
           Contact Us
         </h2>
         <p className={`mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl ${isVisible ? 'slide-from-bottom' : ''}`}>
